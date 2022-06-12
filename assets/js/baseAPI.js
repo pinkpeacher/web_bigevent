2e9a48c02a9f7f9023d3e7346019e0aa83d45b20
grz <1605537551@qq.com>
2022-06-12 15:11:03 +0800

    完成了后台主页的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,5 +3,15 @@
 // 在这个函数中，可以拿到我们给Ajax提供的配置之对象
 $.ajaxPrefilter(function(options){
     options.url='http://www.liulongbin.top:3007'+options.url
-    console.log(options.url); 
+    if(options.url.indexOf(/my/!==-1)){
+        options.headers={
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    options.complete=function (res) {
+        if (res.responseJSON.status==1 && res.responseJSON.message=='身份认证失败！') {
+            localStorage.removeItem("token")
+            location.href = '../../login.html'
+        }
+    }
 })
